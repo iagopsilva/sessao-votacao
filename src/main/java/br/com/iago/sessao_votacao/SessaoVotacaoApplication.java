3854d32ee42cd9921917c1347ec738cde4c07de7
iago <iagopsilva073@gmail.com>
2026-01-10 11:25:32 -0300

    init

---
+++ src/main/java/br/com/iago/sessao_votacao/SessaoVotacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.iago.sessao_votacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessaoVotacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessaoVotacaoApplication.class, args);
+	}
+
+}
